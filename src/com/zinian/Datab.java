46917f1f20c0b7ef37414a26a9ff38bb121dcbf3
zi <1319589118@qq.com>
2017-05-29 18:57:49 +0800

    登录界面完成 注册界面完成

--- src/com/zinian/Datab.java
+++ src/com/zinian/Datab.java
@@ -7,16 +7,17 @@ import java.sql.Connection;
 import java.sql.Statement; 
 public class Datab {
 	
-	public static void main(String[] args) throws Exception {
-        Connection conn = null;
-        String sql;
-        // MySQL的JDBC URL编写方式：jdbc:mysql://主机名称：连接端口/数据库的名称?参数=值
-        // 避免中文乱码要指定useUnicode和characterEncoding
-        // 执行数据库操作之前要在数据库管理系统上创建一个数据库，名字自己定，
-        // 下面语句之前就要先创建javademo数据库
-        String url = "jdbc:mysql://bdm260213172.my3w.com:3306/bdm260213172_db?"
-                + "user=bdm260213172&password=hang183367&useUnicode=true&characterEncoding=UTF8";
-
+	static Connection conn = null;
+    static String sql;
+    static // MySQL的JDBC URL编写方式：jdbc:mysql://主机名称：连接端口/数据库的名称?参数=值
+    // 避免中文乱码要指定useUnicode和characterEncoding
+    // 执行数据库操作之前要在数据库管理系统上创建一个数据库，名字自己定，
+    // 下面语句之前就要先创建javademo数据库
+    String url = "jdbc:mysql://bdm260213172.my3w.com:3306/bdm260213172_db?"
+            + "user=bdm260213172&password=hang183367&useUnicode=true&characterEncoding=UTF8";
+    Statement stmt;
+	Datab() throws Exception {
+        
         try {
             // 之所以要使用下面这条语句，是因为要使用MySQL的驱动，所以我们要把它驱动起来，
             // 可以通过Class.forName把它加载进去，也可以通过初始化来驱动起来，下面三种形式都可以
@@ -26,11 +27,11 @@ public class Datab {
             // or：
             // new com.mysql.jdbc.Driver();
 
-            System.out.println("成功加载MySQL驱动程序");
+            //System.out.println("成功加载MySQL驱动程序");
             // 一个Connection代表一个数据库连接
             conn = DriverManager.getConnection(url);
             // Statement里面带有很多方法，比如executeUpdate可以实现插入，更新和删除等
-            Statement stmt = conn.createStatement();
+            stmt = conn.createStatement();
            // sql = "create table student(NO char(20),name varchar(20),primary key(NO))";
            // int result = stmt.executeUpdate(sql);// executeUpdate语句会返回一个受影响的行数，如果返回-1就没有成功
            // if (result != -1) {
@@ -41,13 +42,13 @@ public class Datab {
 //                sql = "insert into jchatuser(userNO,userPass) values('zinian1','183367')";
 //              //  result = stmt.executeUpdate(sql);
 //                stmt.executeUpdate(sql);
-                sql = "select * from jchatuser";
-                ResultSet rs = stmt.executeQuery(sql);// executeQuery会返回结果的集合，否则返回空值
-                System.out.println("usr\tpass");
-                while (rs.next()) {
-                    System.out
-                            .println(rs.getString(1) + "\t" + rs.getString(2));// 入如果返回的是int类型可以用getInt()
-                }
+//                sql = "select * from jchatuser";
+//                ResultSet rs = stmt.executeQuery(sql);// executeQuery会返回结果的集合，否则返回空值
+//                System.out.println("usr\tpass");
+//                while (rs.next()) {
+//                    System.out
+//                            .println(rs.getString(1) + "\t" + rs.getString(2));// 入如果返回的是int类型可以用getInt()
+//                }
           //  }
         } catch (SQLException e) {
             System.out.println("MySQL操作错误");
@@ -57,8 +58,24 @@ public class Datab {
         } finally {
             conn.close();
         }
+        
 
     }
+	boolean Logindb(String name,String pass){
+		sql = "select userPass form jcharuser where name="+name;
+		boolean flag=false;
+		try {
+			ResultSet rs = this.stmt.executeQuery(sql);
+			if(rs.next()&&rs.getString(1)==pass)
+				flag=true;
+			else
+				flag=false;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return flag;
+	}
 
 //	public static void main(String[] args) {
 //		// TODO Auto-generated method stub
--- src/com/zinian/Theframe.java
+++ src/com/zinian/Theframe.java
@@ -1,17 +1,33 @@
 package com.zinian;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.awt.Window.*;
 import javax.swing.border.Border;
 
 public class Theframe {
-	
+//	static Socket s=null;
+//	static DataOutputStream dos=null;
 	public static void main(String[] args) {
+//		try {
+//			s = new Socket("127.0.0.1",8888);
+//			dos = new DataOutputStream(s.getOutputStream());
+//			mainFrame ff = new mainFrame();
+//			new loginFrame();
+//		} catch (UnknownHostException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
 		// TODO Auto-generated method stub
-		mainFrame ff = new mainFrame();
+//		mainFrame ff = new mainFrame();
 		new loginFrame();
 		
 		
@@ -24,7 +40,21 @@ class mainFrame extends Frame{
 	TextField input;
 	Button bsend,bclear;
 	List friendlist;
+	Socket s=null;
+	DataOutputStream dos=null;
+	DataInputStream dis=null;
 	mainFrame(){
+		try {
+			s = new Socket("127.0.0.1",8888);
+			dos = new DataOutputStream(s.getOutputStream());
+			dis = new DataInputStream(s.getInputStream());
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		pl = new Panel();
 		pr = new Panel();
 		pb = new Panel();
@@ -59,9 +89,9 @@ class mainFrame extends Frame{
 		this.setLocation(400, 300);
 		setVisible(true);
 	}
-	
 	class HandleClose extends WindowAdapter{
 		public void windowClosing(WindowEvent e){
+			disconnect();
 			dispose();
 			System.exit(0);
 		}
@@ -71,6 +101,7 @@ class mainFrame extends Frame{
 
 		Socket s=null;
 		DataOutputStream dos=null;
+		DataInputStream dis=null;
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 			//发送事件
@@ -78,8 +109,6 @@ class mainFrame extends Frame{
 			str=input.getText();
 			if(e.getSource()==bsend){
 				try {
-					s = new Socket("192.168.235.129",8888);
-					dos = new DataOutputStream(s.getOutputStream());
 					dos.writeUTF(str);
 					dos.flush();
 				} catch (IOException e1) {
@@ -96,6 +125,15 @@ class mainFrame extends Frame{
 			}
 		}
 	}
+	void disconnect(){
+		try {
+			dos.close();
+			s.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 }
 class loginFrame extends Frame{
@@ -104,7 +142,22 @@ class loginFrame extends Frame{
 	Button breg,blogin;
 	Label wellabel,lpass,lname;
 	Panel pbut,ppass,pname;
+	Socket s=null;
+	DataOutputStream dos=null;
+	DataInputStream dis = null;
 	loginFrame(){
+		
+		try {
+			s = new Socket("127.0.0.1",8888);
+			dos = new DataOutputStream(s.getOutputStream());
+			dis = new DataInputStream(s.getInputStream());
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 		pbut = new Panel();
 		pass = new TextField(20);
@@ -124,6 +177,7 @@ class loginFrame extends Frame{
 		blogin = new Button("login");
 		breg.addActionListener(new ButtonAction());
 		blogin.addActionListener(new ButtonAction());
+		
 		wellabel = new Label("please input the pass&name!");
 		pbut.setLayout(new GridLayout());
 		pbut.add(breg, BorderLayout.EAST);
@@ -139,26 +193,51 @@ class loginFrame extends Frame{
 		addWindowListener(new HandleClose());
 	}
 	class ButtonAction implements ActionListener{
-
-		@Override
+		
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 			//登录事件
+			String sname=null,spass=null;
+			sname=name.getText();
+			spass=pass.getText();
 			if(e.getSource().equals(blogin)){
-				
-				
-				
+				//System.out.println(dos.toString());
+				try {
+					dos.writeUTF(sname+"@"+spass);
+					dos.flush();
+					if(dis.readBoolean()){
+						 new mainFrame();
+						 dispose();
+					}else{
+						wellabel.setText("用户名或密码错误！");
+					}
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 			}
 			//注册事件
 			else{
 				
 			}
 		}
+		
 	}
 	class HandleClose extends WindowAdapter{
 		public void windowClosing(WindowEvent e){
+			disconnect();
 			dispose();
 			System.exit(0);
 		}
 	}
+	void disconnect(){
+		try {
+			dos.close();
+			s.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
